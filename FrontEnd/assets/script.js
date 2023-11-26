48015b853b99fffc1f2ff417d4d82ffc56a0f17a
Camille Rousset <yuchanx3@gmail.com>
2023-11-26 16:18:29 +0100

    tentative de filter

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -4,32 +4,41 @@ async function Bcategories() {
     const button = await categoriesButtons.json();
     console.log(button)
 
+    button.forEach(buttonVar => {
+        const name = buttonVar.name;
+        const id = buttonVar.id;
+        console.log(id);
 
-
-    button.forEach(element => {
-        const click = element.name;
-
-    document.getElementById("projet").innerHTML +=  `
-    <div>
-        <input src=${click[0]} type="submit" value="Objets"> 
-        <input src=${click[1]} type="submit" value="Appartements">
-        <input src=${click[2]} type="submit" value="Hotels & Restaurants"> 
-    </div>
-    `
+        document.getElementById("filters").innerHTML += `
+            
+            <input type="submit" value="${name}" id="${id}"> 
+            
+        `
+        let buttonElement = document.getElementById(id);
+        buttonElement.addEventListener("click", kaban);
+        buttonElement.id = id;
     });
 }
 Bcategories()
 
-async function kaban() {
+async function kaban(event) {
     const reponse = await fetch("http://localhost:5678/api/works");
-    const projet = await reponse.json();
+    let projet = await reponse.json();
     console.log(projet)
+    console.log(event.target.id)
 
+    if (event != null) {
+        projet = projet.filter((imageProjet) => imageProjet.categoryId == event.target.id)
+    }
+
+
+    document.getElementById("gallery").innerHTML = ` `
     projet.forEach(element => {
+        console.log(element)
         const image = element.imageUrl;
         const title = element.title;
 
-        document.getElementById("gallery").innerHTML +=  `
+        document.getElementById("gallery").innerHTML += `
         <figure>
 				<img src="${image}" alt="${title}">
 				<figcaption>${title}</figcaption>
@@ -39,6 +48,6 @@ async function kaban() {
         console.log(image + "  " + title)
     });
 
-    
+
 }
 kaban()
